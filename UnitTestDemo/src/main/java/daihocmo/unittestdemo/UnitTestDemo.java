15e6b4b8a73b155c60024d1cd431df28a8c31f03
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2025-02-24 13:56:43 +0700

    update feature-test

--- UnitTestDemo/src/main/java/daihocmo/unittestdemo/UnitTestDemo.java
+++ UnitTestDemo/src/main/java/daihocmo/unittestdemo/UnitTestDemo.java
@@ -12,6 +12,6 @@ public class UnitTestDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("TEST BRANCH");
+        System.out.println("TEST 02 BRANCH");
     }
 }
